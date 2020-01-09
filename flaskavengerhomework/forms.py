ba76f021b80cdf921969fd19d717b91f31ee3423
Geovanny Crespo <geovannycrespo@yahoo.comgit config --global user.email geovannycrespo@yahoo.comgit user.emailgit remote add origin https://github.com/geovannycrespo/codingtemple-december-gittest.gitgit push -u origin mastergit push -u origin master>
2020-01-08 19:06:28 -0600

    homework

--- flaskavengerhomework/forms.py
+++ flaskavengerhomework/forms.py
@@ -1,10 +1,13 @@
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, EqualTo,Email
 from flask_wtf import FlaskForm
-
 class SignupForm(FlaskForm):
     avengername = StringField("Avenger's Name", validators=[DataRequired()])
     phonenumber = StringField("Phone Number", validators=[DataRequired()])
     address = StringField("Address", validators=[DataRequired()])
-    
+    submit = SubmitField("Submit")
+
+class LoginForm(FlaskForm):
+    email = StringField("Email",validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Submit")
--- flaskavengerhomework/routes.py
+++ flaskavengerhomework/routes.py
@@ -1,6 +1,12 @@
-from flaskavengerhomework import app 
-from flask import render_template
-from flaskavengerhomework.forms import SignupForm
+from flaskavengerhomework import app, db 
+from flask import render_template, redirect, request, url_for
+from flaskavengerhomework.forms import SignupForm,LoginForm
+
+from werkzeug.security import check_password_hash
+
+from flask_login import login_user, current_user
+
+from flaskavengerhomework.models import User
 
 #home route
 @app.route("/")
@@ -12,10 +18,31 @@ def home():
 @app.route("/signup",methods=["GET","POST"])
 def signup():
     signupForm = SignupForm()
+    if request.method == 'POST':
     
-    avengername = signupForm.avengername.data
-    address = signupForm.address.data
-    phonenumber = signupForm.phonenumber.data
-    print(avengername,phonenumber,address)
+        avengername = signupForm.avengername.data
+        address = signupForm.address.data
+        phonenumber = signupForm.phonenumber.data
+        print(avengername,phonenumber,address)
 
+        user = User(username,email,password)
+        db.session.add(user) # Start Communication with database
+        db.session.commit() # Save Data to database and close session
+    
     return render_template("signup.html", signupform = signupForm)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    loginForm = LoginForm()
+    if request.method == "POST":
+        user_email = loginForm.email.data
+        password = loginForm.password.data
+        # find out who the logged in user currently is
+        logged_user = User.query.filter(User.email == user_email).first()
+        if logged_user and check_password_hash(logged_user.password,password):
+            login_user(logged_user)
+            print(current_user.username)
+            return redirect(url_for('home'))
+        else:
+            print("Not Valid Method")
+    return render_template("login.html", loginform = loginForm)
